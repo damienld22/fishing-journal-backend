db5b1bbcb99bb56b2ef8a39cc1589c14322b372b
Damien Le Dantec <damien.ledantec@telecomsante.com>
2020-09-16 17:11:52 +0200

    Remove HTTPS

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
-const fs = require('fs');
 
 async function bootstrap() {
-  const httpsOptions = {
-    key: fs.readFileSync('/etc/letsencrypt/live/241.ip-92-222-68.eu/privkey.pem'),
-    cert: fs.readFileSync('/etc/letsencrypt/live/241.ip-92-222-68.eu/fullchain.pem')
-  }
-
-  const app = await NestFactory.create(AppModule, { httpsOptions });
+  const app = await NestFactory.create(AppModule);
 
   const options = new DocumentBuilder()
     .setTitle('Fishing journal API')
