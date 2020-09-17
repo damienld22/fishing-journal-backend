d6217041324a99b107077c23fc934a9af01d4fc9
Damien Le Dantec <damien.ledantec@telecomsante.com>
2020-09-17 09:00:56 +0200

    Fix entity too larg

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { urlencoded, json } from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(json({limit: '50mb'}))
+  app.use(urlencoded({extended: true, limit: '50mb'}));
 
   const options = new DocumentBuilder()
     .setTitle('Fishing journal API')
